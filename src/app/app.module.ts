d8835d4458eb45a20ec72e89fe1d7ca274b2085d
biy <2488522344@qq.com>
2020-08-13 17:38:31 +0800

    新增加了内存web这样可以模拟http请求数据，而不是用mock数据了。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 //父传子数据需要的插件
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+// 内存 Web 测试http请求使用
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+// 创建一个内存数据库
+import { InMemoryDataService }  from './service/in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './pages/heroes/heroes.component';
@@ -9,6 +15,7 @@ import { HeroDetailComponent } from './pages/hero-detail/hero-detail.component';
 import { MessagesComponent } from './pages/messages/messages.component';
 import { AppRoutingModule } from './router/app-routing.module';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +28,12 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService,
+      {dataEncapsulation:false},
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 import {Hero} from '../module/hero';
-import {HEROES} from '../mock/mock-heroes';
+// 不需要mock了，用in-memory-data.ts来代替，它就像一个数据库，可以用http来请求。
+// import {HEROES} from '../mock/mock-heroes';
 import { Observable, of} from 'rxjs';
 // 将message服务注入进来
 import {MessageService} from './message.service';
@@ -10,21 +14,59 @@ import {MessageService} from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService:MessageService) { }
+  private heroesUrl = 'api/heroes';
+  httpOptions = {
+    headers:new HttpHeaders({'Content-Type': 'application/json'})
+  }
+
+  constructor(
+    private messageService:MessageService,
+    private http:HttpClient,
+    ) { }
 
   // 添加一个方法，返回模拟的英雄列表
   // getHeroes():Hero[] {
   //   return HEROES;
   // }
+  // 包装messageService方法
+  private log(message:string){
+    this.messageService.add(`HeroService:${message}`);
+  }
+
+  // 错误处理
+  private handleError<T>(operation = 'operation',result?: T){
+    return (error:any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
+  }
+
   // rxjs 中的of函数来模拟从服务器返回的数据。
   getHeroes():Observable<Hero[]>{
-    this.messageService.add(`HeroService:fetched heroes`);
-    return of(HEROES);
+    // return of(HEROES); 改成真正的http请求
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes',[]))
+    )
   }
 
   getHero(id:number):Observable<Hero>{
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    // return of(HEROES.find(hero => hero.id === id));
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
   }
+
+  updateHero(hero:Hero):Observable<any> {
+    return this.http.put(this.heroesUrl,hero,this.httpOptions).pipe(
+      tap( _ => this.log(`updated hero id = ${hero.id}`)),
+      catchError(this.handleError<any>('udapteHero'))
+    )
+  }
+
+
 
 }
